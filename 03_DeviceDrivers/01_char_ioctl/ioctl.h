a59f887fe622f3c720e175e28f48eaad4aefcb71
Pramod Kumar <pramod.kumar20100310@gmail.com>
2025-10-16 15:10:40 +0200

    first base updated

--- 03_DeviceDrivers/01_char_ioctl/ioctl.h
+++ 03_DeviceDrivers/01_char_ioctl/ioctl.h
@@ -1,13 +1,3 @@
-/***************************************************************************
- *                                                                         *
- *      Author          : Ganesh Naik                                      *
- *      Organisation    : Levana Technologies, Mumbai, India.              *
- *      Website		: www.levanatech.in                                *
- *                                                                         *
- *  Conducting Workshops on - Embedded Linux & Device Drivers Training.    *
- *  -------------------------------------------------------------------    *
- *  Tel : 91-9869260324, Email : info@levanatech.in                        *
- *                                                                         *
  ***************************************************************************
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
